b1d313eaa07297a16c4232d74c989b3addc67156
shovon231 <shovon231@gmail.com>
2019-02-11 12:26:25 -0800

    Changes to be committed:

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-let express = require('express')
-let cors = require('cors')
-let bodyParser = require('body-parser')
+let express = require("express")
+let cors = require("cors")
+let bodyParser = require("body-parser")
 let app = express()
 app.use(cors())
-app.use(bodyParser.raw({type: "*/*"}))
+app.use(bodyParser.raw({ type: "*/*" }))
 
+let genarateId = function() {
+  return "" + Math.floor(Math.random() * 100000000)
+}
 
-app.get('/foobar', function(req, res) {
-	res.send("Hello world!")
+let passwords = {}
+let sessions = {}
+
+app.post("/login", function(req, res) {
+  let body = JSON.parse(req.body)
+  let userName = body.userName
+  let enteredPassword = body.password
+  let actualPassword = passwords[userName]
+  if (enteredPassword === actualPassword) {
+    let sessionId = genarateId()
+    res.send(JSON.stringify({ success: true, sid: sessionId }))
+    return
+  }
+  res.send(JSON.stringify({ success: false }))
 })
-app.listen(4000, function() { console.log("Server started on port 4000") })
+
+app.listen(4000, function() {
+  console.log("Server started on port 4000")
+})
